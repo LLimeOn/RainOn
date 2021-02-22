4c015b105e69b0f5dd313494253752eb4dabb050
LLimeOn <78560285+LLimeOn@users.noreply.github.com>
2021-02-22 16:46:42 +0200

    ро

--- main511.py
+++ main511.py
@@ -40,7 +40,7 @@ async def help(ctx):
 serverid = 521678450818547714
 colours = ['#EB5757', '#EB6157', '#EB6A57', '#EB7457', '#EB7E57', '#EB8857', '#EB9257', '#EB9C57', '#EBA657', '#EBB057', '#EBBA57', '#EBC357', '#EBCD57', '#EBD757', '#EBE157', '#EBEB57', '#E1EB57', '#D7EB57', '#CDEB57', '#C3EB57', '#BAEB57', '#B0EB57', '#A6EB57', '#9CEB57', '#92EB57', '#88EB57', '#7EEB57', '#74EB57', '#6AEB57', '#61EB57', '#57EB57', '#57EB61', '#57EB6A', '#57EB74', '#57EB7E', '#57EB88', '#57EB92', '#57EB9C', '#57EBA6', '#57EBB0', '#57EBBA', '#57EBC3', '#57EBCD', '#57EBD7', '#57EBE1', '#57EBEB', '#57E1EB', '#57D7EB', '#57CDEB', '#57C3EB', '#57BAEB', '#57B0EB', '#57A6EB', '#579CEB', '#5792EB', '#5788EB', '#577EEB', '#5774EB', '#576AEB', '#5761EB', '#5757EB', '#6157EB', '#6A57EB', '#7457EB', '#7E57EB', '#8857EB', '#9257EB', '#9C57EB', '#A657EB', '#B057EB', '#BA57EB', '#C357EB', '#CD57EB', '#D757EB', '#E157EB', '#EB57EB', '#EB57E1', '#EB57D7', '#EB57CD', '#EB57C3', '#EB57BA', '#EB57B0', '#EB57A6', '#EB579C', '#EB5792', '#EB5788', '#EB577E', '#EB5774', '#EB576A', '#EB5761']
 rainbowrolename = "Rainbow"
-delay = random.uniform(30, 60))
+delay = random.uniform(3000, 4000))
 
 @Client.command()
 @commands.is_owner()
